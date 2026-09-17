a4fa3d6052d128a92466eb3df569eea5165b0cd3
jdnass <james.d.nassab@gmail.com>
2026-09-17 12:42:50 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='mosa-v21';
+const ASSETS=['./','./index.html','./alif-baa-vocab.js','./dli-basic-data.js','./alkitaab-chapters-5-13.js','./manifest.webmanifest','./icons/moza-icon-180.png','./icons/moza-icon-192.png','./icons/moza-icon-512.png','./icons/moza-icon-1024.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))));
+});
