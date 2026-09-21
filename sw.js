00e811107f5eac9112361b6321f7eac6cfbe2ca3
jdnass <james.d.nassab@gmail.com>
2026-09-20 21:37:25 -0400

    Add new Drive vocabulary audio across study views

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mosa-v23';
+const CACHE='mosa-v24';
 const ASSETS=['./','./index.html','./alif-baa-vocab.js','./dli-basic-data.js','./alkitaab-chapters-5-13.js','./manifest.webmanifest','./icons/moza-icon-180.png','./icons/moza-icon-192.png','./icons/moza-icon-512.png','./icons/moza-icon-1024.png','./audio/vocab/war.m4a','./audio/vocab/american.m4a','./audio/vocab/also.m4a','./audio/vocab/army.m4a'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
