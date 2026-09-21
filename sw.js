8944847600fd88b552132be42a3b86f1b69cac2e
jdnass <james.d.nassab@gmail.com>
2026-09-21 13:04:52 -0400

    Add case analysis cards for Lesson 5 sentences

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='mosa-v26';
-const ASSETS=['./','./index.html','./alif-baa-vocab.js?v=26','./dli-basic-data.js?v=26','./alkitaab-chapters-5-13.js?v=26','./manifest.webmanifest','./icons/moza-icon-180.png','./icons/moza-icon-192.png','./icons/moza-icon-512.png','./icons/moza-icon-1024.png','./audio/vocab/war.m4a','./audio/vocab/american.m4a','./audio/vocab/also.m4a','./audio/vocab/army.m4a'];
+const CACHE='mosa-v27';
+const ASSETS=['./','./index.html','./alif-baa-vocab.js?v=27','./dli-basic-data.js?v=27','./alkitaab-chapters-5-13.js?v=27','./manifest.webmanifest','./icons/moza-icon-180.png','./icons/moza-icon-192.png','./icons/moza-icon-512.png','./icons/moza-icon-1024.png','./audio/vocab/war.m4a','./audio/vocab/american.m4a','./audio/vocab/also.m4a','./audio/vocab/army.m4a'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
